3344d0c33e1502cdabf9aaca845db6e2ed846277
Kevin Orlando <korlando7@gmail.com>
2016-08-23 18:56:16 -0400

    Add Simple-Algorithms

---
+++ Simple-Algorithms/Fibonacci.py
@@ -0,0 +1,26 @@
+#finds nth fibonacci number in the sequence using python3
+import random
+
+n = int(input("Enter number: "))
+
+def fibonacci(n):
+
+	#first two numbers in the sequence
+	a, b = 0, 1
+
+	if n<=1:
+		return n
+
+	for i in range(0, n-1):
+		a, b = b, a+b
+
+	if n % 10 == 1:
+		return ("The " + str(n) + "st number in the Fibonacci sequence is " + str(a))
+	elif n % 10 == 2:
+		return ("The " + str(n) + "nd number in the Fibonacci sequence is " + str(a))
+	elif n % 10 == 3:
+		return ("The " + str(n) + "rd number in the Fibonacci sequence is " + str(a))
+	else:
+		return ("The " + str(n) + "th number in the Fibonacci sequence is " + str(a))
+
+print (fibonacci(n))
