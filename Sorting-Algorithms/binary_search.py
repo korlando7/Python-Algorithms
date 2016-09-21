05f5c9865388ce687b35d242bdcb973adb62ab99
korlando7 <korlando7@gmail.com>
2016-09-21 18:01:41 -0400

    Update Binary Search

--- Sorting-Algorithms/binary_search.py
+++ Sorting-Algorithms/binary_search.py
@@ -1,22 +1,17 @@
 # Uses python3
-import sys
+import random
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
-
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return -1
-
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    m = data[n + 1]
-    a = data[1 : n + 1]
-    for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+def binary_search(a, key):
+    print (a)
+    low = 0
+    high = len(a)-1
+    while low <= high:
+        mid = low + (high-low)//2
+        if a[mid] == key:
+            return mid, key
+        else:
+            if key < a[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+    return False
